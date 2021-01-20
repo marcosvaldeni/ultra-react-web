9c1de6986d9e1f4ad41c6c7a1c35862c6eaba155
Marcos Lucas <marcosvaldeni@gmail.com>
2021-01-20 23:00:34 +0000

    mobile menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 import { 
   Nav, 
   NavbarContainer, 
   NavLogo, 
-  NavIcon 
+  NavIcon,
+  MobileIcon,
+  NavLinks,
+  NavItem,
+  NavMenu
 } from './styles';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
-
+    <IconContext.Provider value={{ color: '#fff' }}>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">
             <NavIcon />
-            Ultra
+            ULTRA
           </NavLogo>
-         
+          <MobileIcon onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
+
+          <NavMenu onClick={handleClick} click={click}>
+            <NavItem>
+              <NavLinks to='/'>Home</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to='/'>Services</NavLinks>
+            </NavItem>
+
+            <NavItem>
+              <NavLinks to='/'>Products</NavLinks>
+            </NavItem>
+          </NavMenu>
+
         </NavbarContainer>
       </Nav>
+    </IconContext.Provider>
 
     </>
   );
